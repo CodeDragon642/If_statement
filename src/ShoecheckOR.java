e20c243ad120f4972a9e3bcbbfa3b38a11a619da
CodeDragon642 <jlejeune642@gmail.com>
2022-11-18 11:59:17 +0000

    now it has the user enter the shoe colour on the shoe check file

--- src/ShoecheckOR.java
+++ src/ShoecheckOR.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class ShoecheckOR {
     public static void main(String[] args) {
-        String shoeColour = "blue";
+        Scanner myObj = new Scanner(System.in);
 
+        System.out.println("Please enter the shoe colour. Make sure to enter it in all lower case!");
+        String shoeColour = myObj.next();
+
+        System.out.println("Is the shoe colour brown or black");
         if (shoeColour == "brown" || shoeColour == "black"){
             System.out.println("Yes!");
         }
